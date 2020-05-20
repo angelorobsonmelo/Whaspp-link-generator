b798ac7e1af4a92e5651ad73386e21787d67c980
angelorobson <helloween123>
2020-05-20 16:41:39 -0300

    Implemented custom spinner

--- app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/linkgenerator/LinkGeneratorFragment.kt
+++ app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/linkgenerator/LinkGeneratorFragment.kt
@@ -3,6 +3,8 @@ package br.com.angelorobson.whatsapplinkgenerator.linkgenerator
 import androidx.fragment.app.Fragment
 import br.com.angelorobson.whatsapplinkgenerator.R
 import br.com.angelorobson.whatsapplinkgenerator.getViewModel
+import br.com.angelorobson.whatsapplinkgenerator.linkgenerator.widgets.CountryAdapter
+import br.com.angelorobson.whatsapplinkgenerator.model.domains.Country
 import com.jakewharton.rxbinding3.view.clicks
 import io.reactivex.Observable
 import io.reactivex.disposables.Disposable
@@ -12,6 +14,7 @@ class LinkGeneratorFragment : Fragment(R.layout.link_generator_fragment) {
 
     lateinit var disposable: Disposable
 
+
     override fun onStart() {
         super.onStart()
 
@@ -20,9 +23,19 @@ class LinkGeneratorFragment : Fragment(R.layout.link_generator_fragment) {
                 ButtonSendClicked
             }
         ).compose(getViewModel(LinkGeneratorViewModel::class).init(Initial))
-            .subscribe {
-                print(it)
+            .subscribe { model ->
+                if (model.linkGeneratorResult is LinkGeneratorResult.CountriesLoaded) {
+                    handleSpinner(model.linkGeneratorResult.countries)
+                }
             }
+    }
+
+    private fun handleSpinner(countries: List<Country>) {
+        val adapter =
+            CountryAdapter(requireContext(), countries)
+        spinnerCountryCode.adapter = adapter
+
+
     }
 
     override fun onDestroy() {
---
+++ app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/linkgenerator/widgets/CountryAdapter.kt
@@ -0,0 +1,54 @@
+package br.com.angelorobson.whatsapplinkgenerator.linkgenerator.widgets
+
+import android.content.Context
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import br.com.angelorobson.whatsapplinkgenerator.R
+import br.com.angelorobson.whatsapplinkgenerator.model.domains.Country
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+
+class CountryAdapter(context: Context, countries: List<Country>) :
+    ArrayAdapter<Country>(context, 0, countries) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return initView(position, convertView, parent)
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return initView(position, convertView, parent)
+    }
+
+    private fun initView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var converterV = convertView
+        if (converterV == null) {
+            converterV = LayoutInflater.from(context).inflate(
+                R.layout.country_spinner_row, parent, false
+            )
+        }
+
+        val imageFlag = converterV?.findViewById<ImageView>(R.id.ivFlag)
+        val textView = converterV?.findViewById<TextView>(R.id.tvCountryName)
+
+        val country = getItem(position)
+
+        val uri =
+            Uri.parse(country?.flag)
+
+        Glide.with(context)
+            .load(uri)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+            .fitCenter()
+            .into(imageFlag!!)
+
+
+        textView?.text = country?.name
+
+        return converterV!!
+    }
+}
