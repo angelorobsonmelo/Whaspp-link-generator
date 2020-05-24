283b17135d957f8dde95e61498748c286fceb11c
angelorobson <helloween123>
2020-05-24 11:25:48 -0300

    remove imports

--- app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/model/repositories/CountryRepository.kt
+++ app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/model/repositories/CountryRepository.kt
@@ -1,10 +1,7 @@
 package br.com.angelorobson.whatsapplinkgenerator.model.repositories
 
 import br.com.angelorobson.whatsapplinkgenerator.model.domains.Country
-import br.com.angelorobson.whatsapplinkgenerator.model.domains.History
 import br.com.angelorobson.whatsapplinkgenerator.model.dtos.CountryDto
-import br.com.angelorobson.whatsapplinkgenerator.model.entities.CountryEntity
-import br.com.angelorobson.whatsapplinkgenerator.model.entities.HistoryEntity
 import br.com.angelorobson.whatsapplinkgenerator.model.services.CountryService
 import io.reactivex.Observable
 import javax.inject.Inject
