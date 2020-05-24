709579c201191e60b629bfe19778575159bb2603
angelorobson <helloween123>
2020-05-24 12:01:45 -0300

    implementing history items

---
+++ app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/ui/history/HistoryFragment.kt
@@ -0,0 +1,14 @@
+package br.com.angelorobson.whatsapplinkgenerator.ui.history
+
+import androidx.fragment.app.Fragment
+import br.com.angelorobson.whatsapplinkgenerator.R
+
+
+class HistoryFragment : Fragment(R.layout.fragment_history) {
+
+    override fun onStart() {
+        super.onStart()
+
+    }
+
+}
