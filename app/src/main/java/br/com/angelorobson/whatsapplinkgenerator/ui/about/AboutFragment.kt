ac46db86efbeea642b047e563887fc0ccac0d1f8
angelorobson <helloween123>
2020-05-27 10:45:06 -0300

    finished app

---
+++ app/src/main/java/br/com/angelorobson/whatsapplinkgenerator/ui/about/AboutFragment.kt
@@ -0,0 +1,56 @@
+package br.com.angelorobson.whatsapplinkgenerator.ui.about
+
+import android.content.Intent
+import android.net.Uri
+import androidx.fragment.app.Fragment
+import br.com.angelorobson.whatsapplinkgenerator.R
+import com.vansuita.materialabout.builder.AboutBuilder
+import kotlinx.android.synthetic.main.fragment_about.*
+
+
+class AboutFragment : Fragment(R.layout.fragment_about) {
+
+    override fun onStart() {
+        super.onStart()
+        setUpAboutScreen()
+    }
+
+    private fun setUpAboutScreen() {
+        val frameLayout = about
+
+        val builder = AboutBuilder.with(requireContext())
+            .setAppIcon(R.mipmap.ic_launcher)
+            .setAppName(R.string.app_name)
+            .setPhoto(R.drawable.angelo)
+            .setCover(R.mipmap.profile_cover)
+            .setLinksAnimated(true)
+            .setDividerDashGap(13)
+            .setName("Ângelo Robson")
+            .setSubTitle(getString(R.string.profession))
+            .setLinksColumnsCount(4)
+            .setBrief(getString(R.string.brief))
+            .addGitHubLink("angelorobsonmelo")
+            .addLinkedInLink("ângelo-melo-8b4a47148/")
+            .addEmailLink("angelorobsonmelo@gmail.com")
+            .addWhatsappLink("Ângelo Robson", "+5582991228122")
+            .setVersionNameAsAppSubTitle()
+            .addShareAction(R.string.app_name)
+            .setActionsColumnsCount(2)
+            .setWrapScrollView(true)
+            .setShowAsCard(true)
+
+        val view = builder.build()
+
+        frameLayout.addView(view)
+
+        val item = builder.lastLinkId
+
+        view.findItem(item).setOnClickListener {
+            val url =
+                "https://api.whatsapp.com/send?phone=5582991228122&text="
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+    }
+
+}
