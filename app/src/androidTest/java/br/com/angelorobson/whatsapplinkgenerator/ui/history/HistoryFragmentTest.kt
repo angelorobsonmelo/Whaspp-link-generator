075c18a87960e0ef01451865a23f915c8679dbf6
angelorobson <helloween123>
2020-05-28 16:34:40 -0300

    Finished history test

--- app/src/androidTest/java/br/com/angelorobson/whatsapplinkgenerator/ui/history/HistoryFragmentTest.kt
+++ app/src/androidTest/java/br/com/angelorobson/whatsapplinkgenerator/ui/history/HistoryFragmentTest.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -17,6 +18,7 @@ import br.com.angelorobson.whatsapplinkgenerator.model.entities.HistoryEntity
 import br.com.angelorobson.whatsapplinkgenerator.ui.component
 import br.com.angelorobson.whatsapplinkgenerator.ui.utils.ActivityService
 import br.com.angelorobson.whatsapplinkgenerator.utils.TestIdlingResource
+import br.com.angelorobson.whatsapplinkgenerator.utils.isToast
 import br.com.angelorobson.whatsapplinkgenerator.utils.withRecyclerView
 import org.junit.After
 import org.junit.Before
@@ -115,5 +117,30 @@ class HistoryFragmentTest {
 
         onView(withRecyclerView(R.id.recyclerView).atPositionOnView(0, R.id.tvMessage))
             .check(matches(withText(message)))
+    }
+
+    // If you are running tests on an emulator, comment on this test block and use the test block below.
+    /* @Test
+     fun clickIconSend_openWhatsAppIfInstalled() {
+         Intents.init()
+
+        onView(withRecyclerView(R.id.recyclerView).atPositionOnView(0, R.id.ivSend))
+            .perform(click())
+
+         intended(hasAction("android.intent.action.VIEW"))
+         intended(hasPackage("com.whatsapp"))
+
+         Intents.release()
+     }*/
+
+
+    // If you are running the tests on a real device with whatsapp installed use this, If not, comment this line and use the test above
+    @Test
+    fun clickIconSend_showWhatsAppNotInstalledToast() {
+        onView(withRecyclerView(R.id.recyclerView).atPositionOnView(0, R.id.ivSend))
+            .perform(click())
+
+        onView(withText(R.string.whatApp_not_installed)).inRoot(isToast())
+            .check(matches(isDisplayed()));
     }
 }
